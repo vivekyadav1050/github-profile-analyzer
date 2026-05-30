5f98e85eb824a70ff810161cd3bc7fea8e4ca24e
vivekyadav1050 <viveksinghald1050@gmail.com>
2026-05-30 22:06:11 +0530

    Add Postman collection

--- Server.js
+++ Server.js
@@ -5,6 +5,11 @@ import app from "./src/app.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Github analyzer API");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
